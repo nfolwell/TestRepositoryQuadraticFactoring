ea09e780c2a44f070785c0398aa5d573a486a03d
BTHS Java A Class - Mr. F <nfolwell@schools.nyc.gov>
2023-10-27 19:23:43 +0000

    Simple parse of quadratic expression

--- QuadraticRunner.java
+++ QuadraticRunner.java
@@ -1,7 +1,17 @@
+import java.util.Scanner;
 public class QuadraticRunner
 {
     public static void main(String[] args)
     {
-        System.out.println("Hi Quadratic Runner!");
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the quadratic expression you'd like to factor.");
+        System.out.print("Enter it in the form ax^2+bx+c: ");
+        String eqn = s.nextLine();
+        int a = Integer.parseInt(eqn.substring(0,eqn.indexOf("x^2")));
+        System.out.println("a = "+a);
+        int b = Integer.parseInt(eqn.substring(eqn.indexOf("+")+1, eqn.indexOf("x+")));
+        System.out.println("b = "+b);
+        int c = Integer.parseInt(eqn.substring(eqn.indexOf("x+")+2));
+        System.out.println("c = "+c);
     }
 }
